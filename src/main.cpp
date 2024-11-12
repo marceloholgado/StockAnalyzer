3314afd2670fac96ff12d52890794b534879098e
Marcelo Holgado <holgadomarcelo184@gmail.com>
2024-11-11 23:48:17 -0300

    finished envoriment setup on windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <gtkmm.h>
 
+int main(int argc, char** argv) {
 
-int main() {
+    std::cout << "Begin" << std::endl;
+    auto app = Gtk::Application::create(argc, argv);
+
+    Gtk::Window window;
+    window.set_default_size(600,400);
+
+    return app->run(window);
 }
